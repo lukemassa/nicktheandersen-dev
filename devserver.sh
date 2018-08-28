1eb0bd38f74ac03ce06a61208ebe48d56466496e
Luke Massa <lmassa@tripadvisor.com>
2018-08-28 00:59:51 -0400

    Meh tried something

---
+++ devserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run the server in dev for testing
+
+cd docs
+python -m SimpleHTTPServer 7000
---
+++ docs/js/main.js
@@ -0,0 +1,68 @@
+(function() {
+
+
+    Vue.component('nta-header',{
+        template:`
+        <div class='title'>
+        nick andersen
+        </div>
+        `
+    });
+    Vue.component('nta-app', {
+        template: `<div>
+        <nta-piano v-if="page=='piano'" @toabout="toabout"></nta-piano>
+        <nta-aboutme v-if="page=='about'" @topiano="topiano"></nta-aboutme>
+        </div>`,
+        data: function() {
+            return {
+                page: "piano"
+            }
+        },
+        methods: {
+            toabout: function() {
+                this.page = "about"
+            },
+            topiano: function() {
+                this.page = "piano"
+            }
+        }
+    });
+    Vue.component('nta-aboutme', {
+		template: `<div>
+        <br>
+        <br>
+        <p>Lorem Ipsum</p>
+        <br>
+        <a class='nav' href="#" v-on:click="topiano()">back.</a>
+		</div>`,
+        methods: {
+            topiano: function() {
+                this.$emit('topiano')
+            }
+        }
+	})
+	
+    Vue.component('nta-piano', {
+        template: `<div>
+        <div class="middlestuff">
+        <div class='icons'><a class='nav' href="#" v-on:click="toabout()">about.</a></p></div>
+        <div class='piano'></div>
+		<div class="icons">
+        <p align='right'>
+		<a href="https://twitter.com/nicktheandersen" target="_blank" ><img title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter1.png" alt="Twitter" width="35" height="35" scale="0"></a>
+		<a href="https://instagram.com/nicktheandersen" target="_blank"><img title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram1.png" alt="Instagram" width="35" height="35" scale="0"></a>
+        </p>
+		</div>
+        </div>
+        </div>`,
+        methods: {
+            toabout: function() {
+                this.$emit('toabout')
+            }
+        }
+    })
+
+
+    app = new Vue({el:"#app"})
+
+})();
