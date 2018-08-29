afa3065a009b013a575e5a55226661c45f5bcdef
Luke Massa <lmassa@tripadvisor.com>
2018-08-28 22:49:57 -0400

    Swap out pic for insta thing

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,17 @@
 (function() {
 
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i == 0 ) return this;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     temp = this[i];
+     this[i] = this[j];
+     this[j] = temp;
+  }
+  return this;
+}
+
 
     Vue.component('nta-header',{
         template:`
@@ -46,19 +58,47 @@
         template: `<div>
         <div class="middlestuff">
         <div class='icons'><a class='nav' href="#" v-on:click="toabout()">about</a></p></div>
-        <div class='piano'></div>
+        <div class='piano'>
+        <div v-for="i in images"><img class='instaimg' v-bind:src="i"></div>
+        </div>
 		<div class="icons">
         <p align='right'>
-		<a href="https://twitter.com/nicktheandersen" target="_blank" ><img title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter1.png" alt="Twitter" width="35" height="35" scale="0"></a>
-		<a href="https://instagram.com/nicktheandersen" target="_blank"><img title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram1.png" alt="Instagram" width="35" height="35" scale="0"></a>
+		<a href="https://twitter.com/nicktheandersen" target="_blank" ><img class="theicons" title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter1.png" alt="Twitter" width="35" height="35" scale="0"></a>
+		<a href="https://instagram.com/nicktheandersen" target="_blank"><img class="theicons" title="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram1.png" alt="Instagram" width="35" height="35" scale="0"></a>
         </p>
 		</div>
         </div>
         </div>`,
+        data: function() {
+            return {
+                images: []
+            }
+        },
         methods: {
             toabout: function() {
                 this.$emit('toabout')
+            },
+            update_images: function() {
+                jQuery.ajax('https://instagram.com/nicktheandersen').done((response) => {
+                    response = response.replace(/\n/g,' ');
+                    // Fingers crossed that instagram doesn't have some XSS bullshit in here...
+                    var match = response.match(/window._sharedData = (.*?);<\/script>/);
+                    var data = JSON.parse(match[1]);
+                    // Oh yeah, there's *no* way this will ever break
+                    // I apologize
+                    var edges = data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
+					edges.shuffle();
+					edges.forEach( (edge) => {
+                        if (this.images.length < 10 ) {
+                            if (edge.node.dimensions.height > 1070 && edge.node.dimensions.height < 1090)
+                            this.images.push(edge.node.display_url);
+                        }
+                    });
+                })
             }
+        },
+        mounted: function() {
+            this.update_images();
         }
     })
 
